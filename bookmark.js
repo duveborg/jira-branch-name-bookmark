d285d9242b2313f4edd72b721ad270be02f6333b
Martin Duveborg <duveborg@gmail.com>
2022-02-03 15:13:41 +0100

    first commit

---
+++ bookmark.js
@@ -0,0 +1,16 @@
+(function () {
+  var issueTitle = document.querySelector(
+    '[data-test-id="issue.views.issue-base.foundation.summary.heading"]'
+  ).innerText;
+  var issueId = document.querySelector(
+    '[data-test-id="issue.views.issue-base.foundation.breadcrumbs.breadcrumb-current-issue-container"]'
+  ).innerText;
+
+  var sluggifiedTitle = issueTitle
+    .toLowerCase()
+    .replace(/ /g, '-')
+    .replace(/[^\w-]+/g, '');
+
+  var branchName = issueId + '-' + sluggifiedTitle;
+  alert(branchName);
+})();
